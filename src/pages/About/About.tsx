e7da8bdfb5969625803c9b60c7150516ae22d53b
rMaxBarros <rmaxbarros@gmail.com>
2025-07-04 17:34:05 -0300

    update: project postponed but i will post it!

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,7 +3,20 @@ import styles from './About.module.css';
 const About = () => {
     return (
         <main className={styles.main}>
-            <h1 className={styles.title}>Sobre mim</h1>
+            <h1 className={styles.title}>Oi! Obrigado por estar aqui</h1>
+            <h2>Essa tela ainda está em construção! O projeto tá bacana né?</h2>
+            <p>
+                Estou realizando alguns projetos que surgiram mas logo termino aqui! (De verdade!)
+            </p>
+            <p>Última atualização: 04/07/2025</p>
+            <p>Você pode acessar o repositório desse projeto clicando no botão abaixo:</p>
+            <a
+                className={styles.button}
+                href='https://github.com/rMaxBarros/meu-portfolio'
+                target='_blank'
+            >
+                Projeto no GitHub
+            </a>
         </main>
     );
 };
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -3,7 +3,11 @@ import styles from './Contact.module.css';
 const Contact = () => {
     return (
         <main className={styles.main}>
-            <h1 className={styles.title}>Contato</h1>
+            <h1 className={styles.title}>Oi! Obrigado por estar aqui</h1>
+            <h2>Essa tela ainda está em construção! O projeto tá bacana né?</h2>
+            <p>
+                Acesse a aba "about me" para mais informações!
+            </p>
         </main>
     );
 };
